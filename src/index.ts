31018fbe4a3a862bb059e4a25a11f4dc761800b0
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-07-24 15:28:44 +0530

    routes init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from './app';
 import dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/.env' });
-const PORT = Number(process.env.PORT) || 3000;
+
+const PORT = Number(process.env.PORT);
 
 app.listen(PORT, () => {
   console.log(`Server started @ ${PORT}`);
